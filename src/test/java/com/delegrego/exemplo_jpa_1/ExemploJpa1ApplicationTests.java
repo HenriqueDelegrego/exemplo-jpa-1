d165c3616befcd4a9962a66f2821219fb31564d3
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-11-04 17:03:54 -0300

    Initial commit

---
+++ src/test/java/com/delegrego/exemplo_jpa_1/ExemploJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delegrego.exemplo_jpa_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
